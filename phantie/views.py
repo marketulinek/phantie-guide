0305d59dea026490e2f017087d572d28b7062868
MarketaH <habranova.marketa@gmail.com>
2023-11-21 22:29:21 +0100

    Apply the repo analyzer form on the homepage

--- phantie/views.py
+++ phantie/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
 
+from .analyzers import RepoAnalyzer
+from .forms import RepoAnalyzerForm
+
 
 def homepage(request):
+    repo_results = []
+    form_init = {}
+    form_user_input_value = request.GET.get('username_repo', None)
 
-    context = {}
+    if form_user_input_value:
+        form_init = {'username_repo': form_user_input_value}
+        repo_results = RepoAnalyzer(form_user_input_value).analyze()
+
+    form = RepoAnalyzerForm(initial=form_init)
+    context = {'form': form, 'repo_results': repo_results}
     return render(request, 'home.html', context)
