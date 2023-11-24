d2d43c3ef81e6a8abe6654a69cfc04634c28cc90
MarketaH <habranova.marketa@gmail.com>
2023-11-24 23:17:38 +0100

    Reformat the code to be better organized and have more readability

--- phantie/analyzers.py
+++ phantie/analyzers.py
@@ -1,84 +1,93 @@
-import phantie.result_types as result_types
 import requests
 
 
 class RepoAnalyzer:
+    """
+    RepoAnalyzer is a class for analyzing GitHub repositories.
+
+    Usage:
+    - Create an instance of RepoAnalyzer with a GitHub username/repo as a parameter.
+    - Call the analyze method to check various aspects of the repository.
+
+    Example:
+    analyzer = RepoAnalyzer("username/repo")
+    results = analyzer.analyze()
+    """
+    GITHUB_API_URL = 'https://api.github.com/repos/{}'
+    URL_RATE_LIMIT = 'https://api.github.com/rate_limit'
+    HEADERS = {'content-type': 'application/json'}
+
     def __init__(self, username_repo):
-        self.headers = {'content-type': 'application/json'}
-        self.github_api_url = f'https://api.github.com/repos/{username_repo}'
-        self.url_rate_limit = 'https://api.github.com/rate_limit'
+        self.username_repo = username_repo
         self.results = []
+        self.errors = []
 
     def analyze(self):
-        if not self._rate_limit_ok():
-            self._fill_results(
-                result_types.NotWorkingResultType,
-                'GitHub API rate limit exceeded',
-                'Please try again later.'
-            )
+        try:
+            if not self._rate_limit_ok():
+                self._save_error('GitHub API rate limit exceeded')
+                return False
+
+            response = self._make_api_request(self.GITHUB_API_URL.format(self.username_repo))
+            if response.status_code == 404:
+                self._save_error('Repository not found')
+                return False
+
+            response.raise_for_status()
+            repo_data = response.json()
+
+            # Check basic info
+            self._check_fork(repo_data['fork'])
+            self._check_description(repo_data['description'])
+            self._check_homepage(repo_data['homepage'])
+            self._check_open_issues_pr(repo_data['open_issues_count'])
+
             return self.results
-
-        response = requests.get(self.github_api_url, headers=self.headers)
-        response.raise_for_status()
-        repo_data = response.json()
-
-        # Check basic info
-        self._check_fork(repo_data['fork'])
-        self._check_description(repo_data['description'])
-        self._check_homepage(repo_data['homepage'])
-        self._check_open_issues_pr(repo_data['open_issues_count'])
-
-        return self.results
+        except Exception as e:
+            print(f"An error occurred: {e}")
+            return False
 
     def _rate_limit_ok(self):
-        r = requests.get(self.url_rate_limit, headers=self.headers)
-        rate_limit = int(r.json()['resources']['core']['remaining'])
-        return rate_limit > 5
+        try:
+            r = self._make_api_request(self.URL_RATE_LIMIT)
+            rate_limit = int(r.json()['resources']['core']['remaining'])
+            return rate_limit > 0
+        except Exception as e:
+            print(f"An error occurred while checking rate limit: {e}")
+            return False
 
-    def _fill_results(self, result_type, label, message=None, links=None):
-        self.results.append({
-            'label': label,
-            'status': result_type.color_class,
-            'message': message,
-            'links': links,
-            'svg_data': result_type.svg_data,
-        })
+    def _make_api_request(self, url):
+        return requests.get(url, headers=self.HEADERS)
+
+    def _save_result(self, label, status='danger', message=None):
+        result = {'label': label, 'status': status}
+        if message:
+            result['message'] = message
+        self.results.append(result)
+
+    def _save_error(self, error_message):
+        self.errors.append(error_message)
 
     def _check_fork(self, value):
         if value:
-            self._fill_results(
-                result_types.BadResultType,
-                'This repository is a fork of another repository',
-                "That's not good."
-            )
+            self._save_result('This repository is a fork of another repository')
 
     def _check_description(self, value):
         if value:
-            self._fill_results(result_types.SuccessResultType, 'Description is ok')
+            self._save_result('Description is ok', status='success')
         else:
-            self._fill_results(
-                result_types.BadResultType,
-                'Description is missing',
-                "That's not good.",
-                'Look here..'
-            )
+            self._save_result('Description is missing',
+                              message='It is a good practice to have a description of your project.')
 
     def _check_homepage(self, value):
         if value:
-            self._fill_results(result_types.SuccessResultType, 'Homepage is ok')
+            self._save_result('Homepage is ok', status='success')
         else:
-            self._fill_results(
-                result_types.BadResultType,
-                'Homepage is missing',
-                "That's not good.",
-                'Look here..'
-            )
+            self._save_result('Homepage is missing',
+                              message='It is a good practice to have a homepage of your project.')
 
     def _check_open_issues_pr(self, open_issues_count):
         if open_issues_count > 0:
-            self._fill_results(
-                result_types.WarningResultType,
-                f"The repository has {open_issues_count} opened issues and/or pull requests"
-            )
+            self._save_result(f"The repository has {open_issues_count} opened issues and/or pull requests")
         else:
-            self._fill_results(result_types.SuccessResultType, 'No open issues or pull requests')
+            self._save_result('No open issues or pull requests', status='success')
--- phantie/views.py
+++ phantie/views.py
@@ -5,14 +5,17 @@ from .forms import RepoAnalyzerForm
 
 
 def homepage(request):
-    repo_results = []
+    results = []
+    errors = []
     form_init = {}
     form_user_input_value = request.GET.get('username_repo', None)
 
     if form_user_input_value:
         form_init = {'username_repo': form_user_input_value}
-        repo_results = RepoAnalyzer(form_user_input_value).analyze()
+        analyzer = RepoAnalyzer(form_user_input_value)
+        results = analyzer.analyze()
+        errors = analyzer.errors
 
     form = RepoAnalyzerForm(initial=form_init)
-    context = {'form': form, 'repo_results': repo_results}
+    context = {'form': form, 'results': results, 'errors': errors}
     return render(request, 'home.html', context)
